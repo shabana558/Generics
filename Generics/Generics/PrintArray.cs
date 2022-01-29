ecd3ab417eb18d9e840cf6be348b8ff2ecca7c02
shaik ameer basha <shaikshabana758@gmail.com>
2022-01-29 13:48:38 +0530

    [shabana] added UC1-PrintArray

---
+++ Generics/Generics/PrintArray.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class PrintArray
+    {
+        public static void toprint(int[] inputArray)
+        {
+            foreach (int element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+           
+        }
+        public static void toprint(double[] inputArray)
+        {
+            foreach (double element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+           
+        }
+        public static void toprint(char[] inputArray)
+        {
+            foreach (char element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            
+
+        }
+
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+            char[] charArray = { 'H', 'A', 'I', };
+
+            PrintArray.toprint(intArray);
+            PrintArray.toprint(doubleArray);
+            PrintArray.toprint(charArray);
+
+        }
+    }
+}
