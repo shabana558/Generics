1cec85b63e5ce77d742276046ff1ff7def8d7742
shaik ameer basha <shaikshabana758@gmail.com>
2022-02-01 16:42:26 +0530

    [shabana] added MaxMethod

--- Generics/Generics/FindMaxGenericclass.cs
+++ Generics/Generics/FindMaxGenericclass.cs
@@ -6,46 +6,38 @@ using System.Threading.Tasks;
 
 namespace Generics
 {
-     public class FindMaxGenericclass<T> where T : IComparable
+     public class GenericMaximum<T> where T : IComparable
     {
-        public T first, second, third;
-
-        private FindMaxGenericclass(T first, T second, T third)
+        public T[] value;
+        public GenericMaximum(T[] value)
         {
-            this.first = first;
-            this.second = second;
-            this.third = third;
+            this.value = value; 
         }
-        private static T FindMaxValueOutofThree(T first, T second, T third)
-        {
-            //result will be either 0 or 1 or -1
-            int res = first.CompareTo(second);
-            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
-            {
-                return first;
-
-            }
-            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
-            {
-                return second;
-            }
-            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
-            {
-                return third;
-            }
-            else
-            {
-
-                Console.WriteLine("either 2 or 3 numbers having same value/equal");
-                return default;
-            }
+        public T[] sort(T[] values)
+        { 
+            Array.Sort(values);
+            return values;
         }
-        public void MaxTest()
+        public T MaxValue(params T[] values)
         {
-            FindMaxValueOutofThree(first, second, third);
-        
+            var sorted_values=sort(values);     
+            return sorted_values[0];
 
         }
+
+        public T MaxMethod()
+        { 
+            var max=MaxValue(this.value);
+            return max;
+
+        }
+        public void PrintMaxValue()
+        {
+            var max=MaxValue(this.value);
+            Console.WriteLine("Maximum value is " + max);
+        }
+
+
     }
 }
 
--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -11,60 +11,65 @@ namespace Generics
 
 
         static void Main(string[] args)
-        {
-            int[] arr = { 10, 20, 30, 40 };
-            double[] doubleArr = { 1.1, 2.2, 3.3, 4.4 };
-            char[] charArr = { 'H', 'A', 'I', 'I' };
-            Console.WriteLine("please enter single alphabet character");
-            //Console.WriteLine(ASCII value is: {0}",console.Read"());//
-            Console.WriteLine("please choose a program to execute");
-            Console.WriteLine("1.Print Arry elements without generic concept.\n 2.Displaying array elements using generic method concept.\n 3.Generic class concept for printing an array.\n 4.Finding maximum value.\n 5.Exit program\n 6.Find max Generic class");
-            Console.ReadLine();
-            int option = Convert.ToInt32(Console.ReadLine());
-            switch (option)
-            {
-                case 1:
-                    //PrintArray.PrintElements(arr);
+        { 
 
-                    //PrintArray.PrintElements(doubleArr);
-                    //PrintArray.PrintElements(charArr);
-                    break;
-                case 2:
-                //Console.WriteLine("\nDisplaying array elements using generic method concept");
-                //GenericsMethodForPrintArray.printElements<int>(arr);
-                //GenericsMethodForPrintArray.PrintElements<double>(doubleArr);
-                //GenericsMethodForPrintArray.printElements<char>(charArr);
-                //break;
+            int[] arr = { 1,2, 3,4};
+        GenericMaximum<int> generic = new GenericMaximum<int>(arr);
+        generic.PrintMaxValue();
+        //{
+        //    int[] arr = { 10, 20, 30, 40 };
+        //    double[] doubleArr = { 1.1, 2.2, 3.3, 4.4 };
+        //    char[] charArr = { 'H', 'A', 'I', 'I' };
+        //    Console.WriteLine("please enter single alphabet character");
+        //    //Console.WriteLine(ASCII value is: {0}",console.Read"());//
+        //    Console.WriteLine("please choose a program to execute");
+        //    Console.WriteLine("1.Print Arry elements without generic concept.\n 2.Displaying array elements using generic method concept.\n 3.Generic class concept for printing an array.\n 4.Finding maximum value.\n 5.Exit program\n 6.Find max Generic class");
+        //    Console.ReadLine();
+        //    int option = Convert.ToInt32(Console.ReadLine());
+        //    switch (option)
+        //    {
+        //        case 1:
+        //            //PrintArray.PrintElements(arr);
 
-                case 3:
-                //Console.WriteLine("Generic class concept for printing an array");
-                //new GenericClassForPrintArray<int>(arr).PrintElements();
-                //new GenericClassForPrintArray<double>(arr).PrintElements();
-                //new GenericClassForPrintArray<char>(arr).PrintElements();
-                case 4:
-                    int x = 60, y = 30, z = 20;
-                    double p = 12.46, q = 43.33, r = 77.23;
-                    string s = "abc", s1 = "pqr", s2 = "xyz";
+        //            //PrintArray.PrintElements(doubleArr);
+        //            //PrintArray.PrintElements(charArr);
+        //            break;
+        //        case 2:
+        //        //Console.WriteLine("\nDisplaying array elements using generic method concept");
+        //        //GenericsMethodForPrintArray.printElements<int>(arr);
+        //        //GenericsMethodForPrintArray.PrintElements<double>(doubleArr);
+        //        //GenericsMethodForPrintArray.printElements<char>(charArr);
+        //        //break;
 
-                    if (FindMax.FindMaxvalueofthree(x, y, z) != 0)
-                        Console.WriteLine("{0} is maximum out of {1},{2},{3}", FindMax.FindMaxvalueofthree(x, y, z), x, y, z);
-                    if (FindMax.FindMaxvalueofthree(p, q, r) != 0.0)
-                        Console.WriteLine("{0} is maximum out of {1},{2},{3}", FindMax.FindMaxvalueofthree(p, q, r), p, q, r);
-                    if (FindMax.FindMaxvalueofthree(x, y, z) != null)
-                        Console.WriteLine("{0} is maximum out of {1},{2},{3}", FindMax.FindMaxvalueofthree(s, s1, s2), s, s1, s2);
+        //        case 3:
+        //        //Console.WriteLine("Generic class concept for printing an array");
+        //        //new GenericClassForPrintArray<int>(arr).PrintElements();
+        //        //new GenericClassForPrintArray<double>(arr).PrintElements();
+        //        //new GenericClassForPrintArray<char>(arr).PrintElements();
+        //        case 4:
+        //            int x = 60, y = 30, z = 20;
+        //            double p = 12.46, q = 43.33, r = 77.23;
+        //            string s = "abc", s1 = "pqr", s2 = "xyz";
 
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    int T1 = 80, T2 = 40, T3 = 50;
-                    FindMaxGeneric Genericclass<int> (T1, T2, T3).MaxTest();
-                    Console. WriteLine("Max Value " + "");
+        //            if (FindMax.FindMaxvalueofthree(x, y, z) != 0)
+        //                Console.WriteLine("{0} is maximum out of {1},{2},{3}", FindMax.FindMaxvalueofthree(x, y, z), x, y, z);
+        //            if (FindMax.FindMaxvalueofthree(p, q, r) != 0.0)
+        //                Console.WriteLine("{0} is maximum out of {1},{2},{3}", FindMax.FindMaxvalueofthree(p, q, r), p, q, r);
+        //            if (FindMax.FindMaxvalueofthree(x, y, z) != null)
+        //                Console.WriteLine("{0} is maximum out of {1},{2},{3}", FindMax.FindMaxvalueofthree(s, s1, s2), s, s1, s2);
 
-                default:
-                    break;
-            }
-                 Console.ReadLine();
+        //            break;
+        //        case 5:
+        //            break;
+        //      ////  case 6:
+        //          //  int T1 = 80, T2 = 40, T3 = 50;
+        //           // FindMaxGeneric Genericclass<int> (T1, T2, T3).MaxTest();
+        //           // Console. WriteLine("Max Value " + "");
+
+        //        default:
+        //            break;
+        //    }
+        //         Console.ReadLine();    
 
 
 
